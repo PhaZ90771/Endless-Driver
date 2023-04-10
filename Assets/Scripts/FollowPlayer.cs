3d3d9c060ed77b6151f85a8022974837e2f622b1
Benjamin Shutt <shuttben@gmail.com>
2023-04-10 12:22:11 -0700

    Setup looping using the portals.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private PlayerController controller;
 
     private void Awake()
     {
@@ -13,10 +14,20 @@ public class FollowPlayer : MonoBehaviour
         {
             Debug.LogError("Missing reference to player GameObject");
         }
+        controller = player.GetComponent<PlayerController>();
     }
 
     private void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var entrance = other.gameObject.GetComponentInParent<EntrancePortal>();
+        if (entrance)
+        {
+            controller.Loop();
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,14 @@ public class PlayerController : MonoBehaviour
     private readonly float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    private EntrancePortal entrancePortal;
+    private ExitPortal exitPortal;
+
+    private void Awake()
+    {
+        entrancePortal = FindAnyObjectByType<EntrancePortal>();
+        exitPortal = FindAnyObjectByType<ExitPortal>();
+    }
 
     private void Update()
     {
@@ -16,5 +24,15 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.forward);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    public void Loop()
+    {
+        // Should warp the player vehicle from the entrance portal at the end of the road to the exit portal at the start of the road, all while the camera is within the portal opening, thus masking the transition.
+        // TODO: Check this is working, number-wise, then setup to trigger when the player camera intersects with the entrance portal opening;
+        var offset = transform.position - entrancePortal.transform.position;
+        transform.position = exitPortal.transform.position + offset;
+
+        //transform.Translate(0, 0, -50);
     }
 }
