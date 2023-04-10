5d1daa31b0da151fe34dc9d3d844cb19d41a20f2
Benjamin Shutt <shuttben@gmail.com>
2023-04-10 12:35:25 -0700

    Removed old code and comments.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,11 +28,7 @@ public class PlayerController : MonoBehaviour
 
     public void Loop()
     {
-        // Should warp the player vehicle from the entrance portal at the end of the road to the exit portal at the start of the road, all while the camera is within the portal opening, thus masking the transition.
-        // TODO: Check this is working, number-wise, then setup to trigger when the player camera intersects with the entrance portal opening;
         var offset = transform.position - entrancePortal.transform.position;
         transform.position = exitPortal.transform.position + offset;
-
-        //transform.Translate(0, 0, -50);
     }
 }
